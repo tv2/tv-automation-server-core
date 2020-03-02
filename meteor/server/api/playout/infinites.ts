89c07c578168165862b4762df340a44d93e57d83
Tom Lee <thomasslee97@gmail.com>
2020-03-02 11:11:48 +0000

    fix: Don't copy infinites to gap pieces

--- meteor/server/api/playout/infinites.ts
+++ meteor/server/api/playout/infinites.ts
@@ -78,6 +78,7 @@ export function updateSourceLayerInfinitesAfterPartInner (rundown: Rundown, prev
 
 	ps = []
 	for (let part of partsToProcess) {
+		if (part.gap) continue
 	   // Drop any that relate only to previous segments
 	   for (let k in activeInfiniteItemsSegmentId) {
 		   let s = activeInfiniteItemsSegmentId[k]
