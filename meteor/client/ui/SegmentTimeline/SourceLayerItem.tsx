5d6059bc16d01a58416af5efac8732db895c2e7a
ianshade <zegzulakrzysztof@gmail.com>
2020-12-16 12:48:47 +0100

    fix: Show script text
    Set the initial elementWidth in componentDidMount to

--- meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
+++ meteor/client/ui/SegmentTimeline/SourceLayerItem.tsx
@@ -66,6 +66,7 @@ interface ISourceLayerItemState {
 export const SourceLayerItem = withTranslation()(
 	class SourceLayerItem extends React.Component<ISourceLayerItemProps & WithTranslation, ISourceLayerItemState> {
 		private _resizeObserver: ResizeObserver | undefined
+		private itemElement: HTMLDivElement | undefined
 
 		constructor(props) {
 			super(props)
@@ -93,6 +94,7 @@ export const SourceLayerItem = withTranslation()(
 			this.setState({
 				itemElement: e,
 			})
+			this.itemElement = e
 		}
 
 		getItemLabelOffsetLeft = (): { [key: string]: string } => {
@@ -420,11 +422,11 @@ export const SourceLayerItem = withTranslation()(
 		}
 
 		private mountResizeObserver() {
-			if (this.props.isLiveLine && !this._resizeObserver && this.state.itemElement) {
+			if (this.props.isLiveLine && !this._resizeObserver && this.itemElement) {
 				this._resizeObserver = new ResizeObserver(this.onResize)
-				this._resizeObserver.observe(this.state.itemElement)
+				this._resizeObserver.observe(this.itemElement)
 
-				const width = getElementWidth(this.state.itemElement) || 0
+				const width = getElementWidth(this.itemElement) || 0
 				if (this.state.elementWidth !== width) {
 					this.setState({
 						elementWidth: width,
@@ -463,6 +465,13 @@ export const SourceLayerItem = withTranslation()(
 		componentDidMount() {
 			if (this.props.isLiveLine) {
 				this.mountResizeObserver()
+			} else if (this.itemElement) {
+				const width = getElementWidth(this.itemElement) || 0
+				if (this.state.elementWidth !== width) {
+					this.setState({
+						elementWidth: width,
+					})
+				}
 			}
 
 			window.addEventListener(RundownViewEvents.highlight, this.onHighlight)
