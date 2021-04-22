abdaea35dccceaa793f54ead094a0cc7736da1c1
Tom Lee <thomasslee97@gmail.com>
2021-04-22 15:39:54 +0100

    dist: get part for piece in adlib actions

--- dist/context.d.ts
+++ dist/context.d.ts
@@ -68,6 +68,8 @@ export interface ActionExecutionContext extends ShowStyleContext {
         excludeCurrentPart?: boolean;
         pieceMetaDataFilter?: any;
     }): IBlueprintPiece | undefined;
+    getPartInstanceForPreviousPiece(piece: IBlueprintPieceInstance): IBlueprintPartInstance;
+    getPartForPreviousPiece(pieceId: string): IBlueprintPart | undefined;
     /** Fetch the showstyle config for the specified part */
     /** Creative actions */
     /** Insert a pieceInstance. Returns id of new PieceInstance. Any timelineObjects will have their ids changed, so are not safe to reference from another piece */
