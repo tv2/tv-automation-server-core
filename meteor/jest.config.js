7366a7c1af3fac66ec824e7b8a6eb78c5d7f136b
Rasmus Lindved <LindvedKrvang@users.noreply.github.com>
2023-07-05 10:00:15 +0200

    Setup simple tests to demostrate mocking dependencies

--- meteor/jest.config.js
+++ meteor/jest.config.js
@@ -11,7 +11,7 @@ const commonConfig = {
 				plugins: [
 					'@babel/plugin-transform-modules-commonjs',
 					// Fibers and await do not work well together. This transpiles await calls to something that works
-					'meteor-babel/plugins/async-await.js',
+					// 'meteor-babel/plugins/async-await.js',
 				],
 			},
 			diagnostics: {
@@ -25,8 +25,9 @@ const commonConfig = {
 		'^.+\\.(js|jsx|mjs)$': path.resolve('./scripts/babel-jest.js'),
 	},
 	transformIgnorePatterns: ['node_modules/(?!(debounce-fn|p-queue|p-timeout|mimic-fn)/)', '\\.pnp\\.[^\\/]+$'],
-	globalSetup: './__mocks__/global-setup.js',
-	setupFilesAfterEnv: ['./__mocks__/_setupMocks.ts'],
+	// globalSetup: './__mocks__/global-setup.js',
+	setupFilesAfterEnv: [],
+	// setupFilesAfterEnv: ['./__mocks__/_setupMocks.ts'],
 	coverageThreshold: {
 		global: {
 			branches: 0,
@@ -77,6 +78,14 @@ module.exports = {
 			testMatch: [
 				'<rootDir>/server/__tests__/**/*.(spec|test).(ts|js)',
 				'<rootDir>/server/**/__tests__/**/*.(spec|test).(ts|js)',
+				'!.meteor/*.*',
+			],
+			testEnvironment: 'node',
+		}),
+		Object.assign({}, commonConfig, {
+			displayName: 'sofie-server',
+			testMatch: [
+				'<rootDir>/sofie-server/**/*.(spec|test).(ts|js)',
 				'!.meteor/*.*',
 			],
 			testEnvironment: 'node',
---
+++ meteor/sofie-server/data-access/repositories/test/cached-rundown-repository.spec.ts
@@ -0,0 +1,38 @@
+import { CachedRundownRepository } from '../cache/cached-rundown-repository'
+import { RundownRepository } from '../interfaces/rundown-repository'
+import { instance, mock, verify, when } from 'ts-mockito'
+import { Rundown, RundownInterface } from '../../../model/entities/rundown'
+
+describe('cached-rundown-repository', () => {
+	describe('getRundown', () => {
+		it('receives a RundownId returns a rundown', async () => {
+			const mockRepo: RundownRepository = mock<RundownRepository>()
+
+			const randomRundownId: string = 'randomRundownId'
+			const randomRundown: Rundown = new Rundown({id: randomRundownId} as RundownInterface)
+
+			when(mockRepo.getRundown(randomRundownId)).thenReturn(Promise.resolve(randomRundown))
+
+			const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo))
+
+			const result: Rundown = await testee.getRundown(randomRundownId)
+			expect(result).toBe(randomRundown)
+		})
+
+		it('receives two request to fetch the same Rundown, only call the database once', async () => {
+			const mockRepo: RundownRepository = mock<RundownRepository>()
+
+			const randomRundownId: string = 'randomRundownId'
+			const randomRundown: Rundown = new Rundown({id: randomRundownId} as RundownInterface)
+
+			when(mockRepo.getRundown(randomRundownId)).thenReturn(Promise.resolve(randomRundown))
+
+			const testee: CachedRundownRepository = new CachedRundownRepository(instance(mockRepo))
+
+			await testee.getRundown(randomRundownId)
+			await testee.getRundown(randomRundownId)
+
+			verify(mockRepo.getRundown(randomRundownId)).once()
+		})
+	})
+})
